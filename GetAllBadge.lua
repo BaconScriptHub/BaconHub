377c8f80801612b1bcda5b5a870be3f6659aaa20
BaconScriptHub <kevinedurantlepard@gmail.com>
2024-05-24 12:51:05 +0800

    Create GetAllBadge.lua

---
+++ GetAllBadge.lua
@@ -0,0 +1,9 @@
+--[[ Txt Obfuscated by Zxveo V3 (final update)
+Credit
+- Creator 
+- Bugs Fixer
+- Scripter
+- Owner
+Yes, i make this Obfuscator with noone
+]]--
+loadstring("\112\114\105\110\116\40\34\32\89\79\85\82\32\83\67\82\73\80\84\32\72\69\82\69\33\33\32\34\41\10")()
